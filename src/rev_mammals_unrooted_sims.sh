ac36ba764cac3fd19f26f2b8298e796b1d7221be
Ben Kaehler <benjamin.kaehler@anu.edu.au>
2016-05-18 16:02:55 +1000

    added reversible sim fit submission script

---
+++ src/rev_mammals_unrooted_sims.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -l wd
+#PBS -q normal
+#PBS -l walltime=5:00:00,mem=2208GB,ncpus=1104
+#PBS -v DB_HOST
+#PBS -P x36
+#usage: map_collection.py [-h]
+#                         (-i INPUT_COLLECTION | -I INPUT_COLLECTIONS_FILE)
+#                         [-b DB_HOST]
+#                         (-o OUTPUT_COLLECTION | -O OUTPUT_COLLECTIONS_FILE)
+#                         -f FUNCTION [-L LOG_LEVEL] [-l LOG_NAME] [-s]
+#                         [-B BATCH_SIZE] [-k KWARGS_FILE] [-m]
+source ~/.profile
+workon normal
+mpiexec -np $PBS_NCPUS python map_collection.py -i mammals.GNC_unrooted_sims -b $DB_HOST -L DEBUG -f ml.ml -o mammals.Y98_unrooted_sims_fits -k ../config/Y98.json -s
+mpiexec -np $PBS_NCPUS python map_collection.py -i mammals.GNC_unrooted_sims -b $DB_HOST -L DEBUG -f ml.ml -o mammals.CNFGTR_unrooted_sims_fits -k ../config/CNFGTR.json -s
