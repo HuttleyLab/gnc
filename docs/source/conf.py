af6b54a34df9a9f125ce8c739936b83fe9c84df1
Ben Kaehler <benjamin.kaehler@anu.edu.au>
2016-07-03 23:05:43 +1000

    what is wrong with readthedocs?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,7 @@ import sys
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
+    'sphinx.ext.coverage', 'sphinx.ext.autodoc', 'sphinx.ext.imgmath',
     'sphinxcontrib.programoutput'
 ]
 
